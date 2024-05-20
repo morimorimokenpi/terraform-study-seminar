cb37933ec188497fad78fd26cfa282c8547741f2
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2024-05-20 12:16:32 +0900

    implement

--- hello.tf
+++ hello.tf
@@ -115,47 +115,47 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "sample_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_internet_gateway" "sample_igw" {
+  vpc_id = aws_vpc.sample_vpc.id
 }
 
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_route_table" "sample_route_table" {
+  vpc_id = aws_vpc.sample_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.sample_igw.id
   }
 }
 
-resource "aws_subnet" "my_subnet_a" {
-  vpc_id            = aws_vpc.my_vpc.id
+resource "aws_subnet" "sample_subnet_a" {
+  vpc_id            = aws_vpc.sample_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-northeast-1a"
 }
 
-resource "aws_subnet" "my_subnet_b" {
-  vpc_id            = aws_vpc.my_vpc.id
+resource "aws_subnet" "sample_subnet_b" {
+  vpc_id            = aws_vpc.sample_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-northeast-1b"
 }
 
-resource "aws_route_table_association" "my_route_table_association_a" {
-  subnet_id      = aws_subnet.my_subnet_a.id
-  route_table_id = aws_route_table.my_route_table.id
+resource "aws_route_table_association" "sample_route_table_association_a" {
+  subnet_id      = aws_subnet.sample_subnet_a.id
+  route_table_id = aws_route_table.sample_route_table.id
 }
 
-resource "aws_route_table_association" "my_route_table_association_b" {
-  subnet_id      = aws_subnet.my_subnet_b.id
-  route_table_id = aws_route_table.my_route_table.id
+resource "aws_route_table_association" "sample_route_table_association_b" {
+  subnet_id      = aws_subnet.sample_subnet_b.id
+  route_table_id = aws_route_table.sample_route_table.id
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "my-ecs-task-execution-role"
+  name = "sample-ecs-task-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -175,12 +175,12 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "my-cluster"
+resource "aws_ecs_cluster" "sample_cluster" {
+  name = "sample-cluster"
 }
 
-resource "aws_ecs_task_definition" "my_task_definition" {
-  family                   = "my-task-family"
+resource "aws_ecs_task_definition" "sample_task_definition" {
+  family                   = "sample-task-family"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -188,7 +188,7 @@ resource "aws_ecs_task_definition" "my_task_definition" {
 
   container_definitions = jsonencode([
     {
-      name      = "my-container"
+      name      = "sample-container"
       image     = "nginx:latest"
       portMappings = [
         {
@@ -202,29 +202,29 @@ resource "aws_ecs_task_definition" "my_task_definition" {
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 }
 
-resource "aws_ecs_service" "my_service" {
-  name            = "my-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task_definition.arn
+resource "aws_ecs_service" "sample_service" {
+  name            = "sample-service"
+  cluster         = aws_ecs_cluster.sample_cluster.id
+  task_definition = aws_ecs_task_definition.sample_task_definition.arn
   desired_count   = 1
 
   launch_type = "FARGATE"
 
   network_configuration {
     assign_public_ip = true
-    subnets         = [aws_subnet.my_subnet_a.id, aws_subnet.my_subnet_b.id]
+    subnets         = [aws_subnet.sample_subnet_a.id, aws_subnet.sample_subnet_b.id]
     security_groups = [aws_security_group.ecs_service_sg.id]
   }
   load_balancer {
-    target_group_arn = aws_lb_target_group.my_target_group.arn
-    container_name   = "my-container"
+    target_group_arn = aws_lb_target_group.sample_target_group.arn
+    container_name   = "sample-container"
     container_port   = 80
   }
 }
 
 resource "aws_security_group" "ecs_service_sg" {
   name        = "ecs-service-sg"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = aws_vpc.sample_vpc.id
 
   ingress {
     from_port   = 80
@@ -242,7 +242,7 @@ resource "aws_security_group" "ecs_service_sg" {
 
 resource "aws_security_group" "lb_sg" {
   name        = "lb-sg"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = aws_vpc.sample_vpc.id
 
   ingress {
     from_port   = 80
@@ -258,31 +258,31 @@ resource "aws_security_group" "lb_sg" {
   }
 }
 
-resource "aws_lb" "my_lb" {
-  name               = "my-lb"
+resource "aws_lb" "sample_lb" {
+  name               = "sample-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.my_subnet_a.id, aws_subnet.my_subnet_b.id]
+  subnets            = [aws_subnet.sample_subnet_a.id, aws_subnet.sample_subnet_b.id]
 
   enable_deletion_protection = false
 }
 
-resource "aws_lb_target_group" "my_target_group" {
-  name        = "my-target-group"
+resource "aws_lb_target_group" "sample_target_group" {
+  name        = "sample-target-group"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = aws_vpc.sample_vpc.id
   target_type = "ip"
 }
 
-resource "aws_lb_listener" "my_listener" {
-  load_balancer_arn = aws_lb.my_lb.arn
+resource "aws_lb_listener" "sample_listener" {
+  load_balancer_arn = aws_lb.sample_lb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my_target_group.arn
+    target_group_arn = aws_lb_target_group.sample_target_group.arn
   }
 }
